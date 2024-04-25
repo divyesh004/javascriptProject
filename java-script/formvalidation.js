0cea6bf7fdd95a4899c4cb122c48bdb2b680bb96
Divyesh Yadav <imdyadav04@gmail.com>
2024-04-25 12:01:47 +0530

    update-2

--- java-script/formvalidation.js
+++ java-script/formvalidation.js
@@ -43,7 +43,7 @@ signupForm.addEventListener("submit", (e) => {
     icon: "success"
   });
   setInterval(() => {
-    window.location.href = "http://127.0.0.1:5501/sign-in.html";
+    window.location.href = "sign-in.html"
   signupForm.reset();
   },1500);
 });
